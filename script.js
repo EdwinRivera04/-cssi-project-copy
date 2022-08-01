2952e3df85cf6505ffeca4a5586c7588ae095bd2
Abrar <abrarhabib285@gamil.com>
2022-08-01 18:49:46 -0400

    api backend more functional

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function generateNewCard(text) {
-    let imgsrc = 'https://picsum.photos/300'
+async function getData() {
+    let url = `https://picsum.photos/id/${Math.floor((Math.random() * 1084) + 1)}/info`
+
+    let response = await fetch(url);
+    let data = await response.json();
+
     let container = document.querySelector(".container");
     container.innerHTML += `
     <div class="row h-100" id="swiping">
         <div class="col d-flex justify-content-center align-items-center h-100">
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <img src="${imgsrc}" class="card-img-top" alt="">
-                    <p class="card-text">${text}</p>
+                    <img src="${data.download_url}" class="card-img-top" alt="${data.author}'s image">
+                    <p class="card-text">${data.author}'s image</p>
                 </div>
             </div>
         </div>
     </div>`;
 }
 
-generateNewCard("hello")
-
+getData();
 
